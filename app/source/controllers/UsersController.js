19947865006326a03387cc02df5f92e78a2c5877
Marcin Kazubowski <marcin.kazubowski@intel.com>
2016-09-27 12:55:08 +0200

    fetch mocked user data

--- app/source/controllers/UsersController.js
+++ app/source/controllers/UsersController.js
@@ -1,6 +1,21 @@
 angular.module('app').controller('UsersController', ['$scope', '$rootScope', 'UsersService', 'UsersModel', function($scope, $rootScope, UsersService, UsersModel) {
     'use strict';
 
-    console.log('test');
+    $scope.init = function () {
+        $rootScope.isLoading = true;
+
+        UsersService.getUsers().then(
+            function success (response) {
+                console.log(response);
+            }, function error (error) {
+                //response comes back after browser's timeout.
+                console.log('check expected mocked data retrieval', error);
+            }
+        ).finally(
+            function () {
+                $rootScope.isLoading = false;
+            }
+        );
+    };
 
 }]);
--- app/source/services/UsersService.js
+++ app/source/services/UsersService.js
@@ -1,4 +1,18 @@
 angular.module('app').service('UsersService', ['$q', 'UsersModel', 'UsersResource', function($q, UsersModel, UsersResource) {
     'use strict';
 
+    this.getUsers = function () {
+        var deferred = $q.defer();
+
+        UsersResource.getUsers(
+            function (data) {
+                deferred.resolve(data);
+            }, function (error) {
+                //mocked data assigned to a failed callback
+                deferred.reject(UsersModel.users);
+            }
+        );
+        return deferred.promise;
+    };
+
 }]);
